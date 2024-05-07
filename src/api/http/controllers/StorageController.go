05c5d8853395bdc035c234a4d92b416e4e719516
Ali Khedmati <ali@khedmati.ir>
2024-05-07 15:22:17 +0330

    enhancements + test deploy.

--- src/api/http/controllers/StorageController.go
+++ src/api/http/controllers/StorageController.go
@@ -3,13 +3,10 @@ package controllers
 import (
 	"cdn/src/api/http/response"
 	"cdn/src/pkg/minio"
-	"fmt"
+	"cdn/src/pkg/utils"
 	"github.com/gin-gonic/gin"
 	minio2 "github.com/minio/minio-go/v7"
-	"mime/multipart"
 	"net/http"
-	"path/filepath"
-	"strings"
 	"time"
 )
 
@@ -17,7 +14,6 @@ var bucketName = "paresh" //todo: hard-coded
 
 type UploadInfo struct {
 	FileName string `json:"fileName"`
-	Size     int64  `json:"size"`
 	URL      string `json:"url"`
 }
 
@@ -34,7 +30,7 @@ func NewStorageController() *StorageController {
 func (storageController *StorageController) PutObject(c *gin.Context) {
 	form, _ := c.MultipartForm()
 
-	err := validateFiles(form.File["files"])
+	err := utils.ValidateFiles(form.File["files"])
 
 	if err != nil {
 		response.Api(c).SetStatusCode(http.StatusUnprocessableEntity).Send()
@@ -51,56 +47,45 @@ func (storageController *StorageController) PutObject(c *gin.Context) {
 		}
 		defer src.Close()
 
-		_, err = storageController.minio.GetMinio().PutObject(c, bucketName, file.Filename, src, file.Size, minio2.PutObjectOptions{
+		uuidFileName := utils.GenerateUUIDFileName(file.Filename)
+
+		_, err = storageController.minio.GetMinio().PutObject(c, bucketName, uuidFileName, src, file.Size, minio2.PutObjectOptions{
 			ContentType: file.Header.Get("Content-Type"),
 		})
 
 		if err != nil {
-			response.Api(c).SetMessage(err.Error()).SetStatusCode(http.StatusInternalServerError).Send()
+			response.Api(c).
+				SetMessage(err.Error()).
+				SetStatusCode(http.StatusInternalServerError).
+				Send()
+
 			return
 		}
 
-		presignedURL, err := storageController.minio.GetMinio().PresignedGetObject(c, bucketName, file.Filename, 8*time.Hour, nil)
+		presignedURL, err := storageController.minio.GetMinio().PresignedGetObject(c, bucketName, uuidFileName, 8*time.Hour, nil)
 
 		if err != nil {
-			response.Api(c).SetMessage(err.Error()).SetStatusCode(http.StatusInternalServerError).Send()
+			response.Api(c).
+				SetMessage(err.Error()).
+				SetStatusCode(http.StatusInternalServerError).
+				Send()
+
 			return
 		}
 
 		uploadInfoList = append(uploadInfoList, UploadInfo{
-			FileName: file.Filename,
-			Size:     file.Size,
+			FileName: uuidFileName,
 			URL:      presignedURL.String(),
 		})
 	}
 
-	response.Api(c).SetMessage("Files uploaded successfully").SetStatusCode(http.StatusOK).SetData(map[string]interface{}{
-		"uploads": uploadInfoList,
-	}).Send()
+	response.Api(c).
+		SetMessage("Files uploaded successfully").
+		SetStatusCode(http.StatusOK).
+		SetData(map[string]interface{}{
+			"uploads": uploadInfoList,
+		}).Send()
 
 	return
 
-}
-
-// Custom validation function for uploaded files
-func validateFiles(files []*multipart.FileHeader) error {
-	// Allowed file extensions todo: hard-coded
-	allowedExts := []string{".jpg", ".jpeg", ".png", ".pdf", "zip", "rar", "docx", "doc", "csv", "xlsx", "mkv", "mp4"}
-
-	for _, file := range files {
-		ext := strings.ToLower(strings.TrimSpace(filepath.Ext(file.Filename)))
-		// Check if file extension is allowed
-		valid := false
-		for _, allowedExt := range allowedExts {
-			if ext == allowedExt {
-				valid = true
-				break
-			}
-		}
-		if !valid {
-			return fmt.Errorf("invalid file format: %s", file.Filename)
-		}
-	}
-
-	return nil
 }
---
+++ src/pkg/utils/utils.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"mime/multipart"
+	"path/filepath"
+	"strings"
+)
+
+func ValidateFiles(files []*multipart.FileHeader) error {
+	allowedExts := []string{".jpg", ".jpeg", ".png", ".pdf", "zip", "rar", "docx", "doc", "csv", "xlsx", "mkv", "mp4"}
+
+	for _, file := range files {
+		ext := strings.ToLower(strings.TrimSpace(filepath.Ext(file.Filename)))
+		// Check if file extension is allowed
+		valid := false
+		for _, allowedExt := range allowedExts {
+			if ext == allowedExt {
+				valid = true
+				break
+			}
+		}
+		if !valid {
+			return fmt.Errorf("invalid file format: %s", file.Filename)
+		}
+	}
+
+	return nil
+}
+
+func GenerateUUIDFileName(originalFileName string) string {
+	ext := filepath.Ext(originalFileName)
+	uuidFileName := uuid.NewString() + ext
+	return uuidFileName
+}
