14a5d52c1411d3ff2808ac7053f94bfc1223d7d2
Amirhossein Motaghian <a.motaghian@esavis.ir>
2024-10-12 15:46:19 +0330

    add apikey and allow ip

---
+++ src/api/http/middlewares/allow_ip.go
@@ -0,0 +1,51 @@
+package middlewares
+
+import (
+	"cdn/src/api/http/response"
+	"cdn/src/config"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+type IPMiddleware struct {
+	allowOrigin []string
+}
+
+func NewIPMiddleware() *IPMiddleware {
+	allowOriginStr := config.GetInstance().Get("ALLOW_ORIGINS")
+	allowOrigins := strings.Split(allowOriginStr, ",")
+
+	return &IPMiddleware{
+		allowOrigin: allowOrigins,
+	}
+}
+
+// Middleware checks if the client's IP is in the allowed list
+func (middleware *IPMiddleware) Middleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ip := c.Request.RemoteAddr
+		if forwarded := c.Request.Header.Get("X-Forwarded-For"); forwarded != "" {
+			ip = strings.Split(forwarded, ",")[0]
+		}
+
+		allowed := false
+		for _, allowedIP := range middleware.allowOrigin {
+			if ip == allowedIP {
+				allowed = true
+				break
+			}
+		}
+
+		if !allowed {
+			response.Api(c).
+				SetStatusCode(http.StatusForbidden).
+				SetMessage("forbidden: Your IP is not allowed").
+				Send()
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
